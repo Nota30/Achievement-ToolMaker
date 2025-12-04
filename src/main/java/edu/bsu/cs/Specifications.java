0b7f153d62c4829db220028fdd40eeebf6d1d71b
Nota30 <harenkeshaav11@gmail.com>
2025-12-03 22:14:01 -0500

    Add specifications class with requirements for each grade

---
+++ src/main/java/edu/bsu/cs/Specifications.java
@@ -0,0 +1,33 @@
+package edu.bsu.cs;
+
+public class Specifications {
+    public static final int ASSIGNMENTS_A = 7;
+    public static final int ASSIGNMENTS_B = 6;
+    public static final int ASSIGNMENTS_C = 5;
+    public static final int ASSIGNMENTS_D = 4;
+
+    public static final int ACHIEVEMENTS_A = 4;
+    public static final int ACHIEVEMENTS_B = 3;
+    public static final int ACHIEVEMENTS_C = 2;
+    public static final int ACHIEVEMENTS_D = 1;
+
+    public static final Performance MIDTERM_REPORT_A = Performance.FULL;
+    public static final Performance MIDTERM_REPORT_B = Performance.FULL;
+    public static final Performance MIDTERM_REPORT_C = Performance.FULL;
+    public static final Performance MIDTERM_REPORT_D = Performance.OPTIONAL;
+
+    public static final Performance FINAL_EXAM_A = Performance.FULL;
+    public static final Performance FINAL_EXAM_B = Performance.NEARLY;
+    public static final Performance FINAL_EXAM_C = Performance.MINIMUM;
+    public static final Performance FINAL_EXAM_D = Performance.OPTIONAL;
+
+    public static final ProjectLevel FIRST_PROJECT_A = ProjectLevel.STARTER;
+    public static final ProjectLevel FIRST_PROJECT_B = ProjectLevel.UNASSESSABLE;
+    public static final ProjectLevel FIRST_PROJECT_C = ProjectLevel.UNASSESSABLE;
+    public static final ProjectLevel FIRST_PROJECT_D = ProjectLevel.UNASSESSABLE;
+
+    public static final ProjectLevel FINAL_PROJECT_A = ProjectLevel.MASTER;
+    public static final ProjectLevel FINAL_PROJECT_B = ProjectLevel.PROFICIENT;
+    public static final ProjectLevel FINAL_PROJECT_C = ProjectLevel.STARTER;
+    public static final ProjectLevel FINAL_PROJECT_D = ProjectLevel.STARTER;
+}
